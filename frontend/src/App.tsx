5e1d5259da44bf5444df23d70ff35e6e6ebaffb8
Rizx <157099086+rizx-r@users.noreply.github.com>
2026-06-07 16:05:21 +0800

    refactor: replace old routing with PipelineView, remove old pages and components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import { UploadPage } from './pages/UploadPage';
-import { CharacterEditor } from './pages/CharacterEditor';
-import { SceneEditor } from './pages/SceneEditor';
-import { EpisodePlanner } from './pages/EpisodePlanner';
-import { ScriptPreview } from './pages/ScriptPreview';
+import { JobProvider } from './context/JobContext';
+import { PipelineView } from './components/PipelineView';
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<UploadPage />} />
-          <Route path="job/:jobId/characters" element={<CharacterEditor />} />
-          <Route path="job/:jobId/scenes" element={<SceneEditor />} />
-          <Route path="job/:jobId/episodes" element={<EpisodePlanner />} />
-          <Route path="job/:jobId/script" element={<ScriptPreview />} />
-          <Route path="*" element={<div>Page not found</div>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <JobProvider>
+      <PipelineView />
+    </JobProvider>
   );
 }
